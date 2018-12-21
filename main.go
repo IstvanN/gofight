590f5150e637a40d0e673cae34ba355b70006ed8
Istvan Nemeth <nemethistvan.ius@gmail.com>
2018-12-21 20:07:10 +0100

    fix(main): delete gofight and rename to main.go

--- main.go
+++ main.go
@@ -3,19 +3,22 @@ package main
 import (
 	"fmt"
 
-	"github.com/IstvanN/gofight/classes"
+	. "github.com/IstvanN/gofight/classes"
 )
 
 func main() {
 	fmt.Println("Welcome to GoFight!")
-	var f1 classes.Fighter = classes.CreateRogue("Wolfgraff")
-	var f2 classes.Fighter = classes.CreateRogue("Grunt")
-	fmt.Println(f1, f2)
-	f1.Act(f2)
-	f2.Act(f1)
-	fmt.Println(f1, f2)
+	var f1 Fighter = CreateRogue("Wolfgraff")
+	var f2 Fighter = CreateRogue("Grunt")
+	arena(f1, f2)
 }
 
-func arena(f1 classes.Fighter, f2 classes.Fighter) {
-	fmt.Printf("Welcome to the Arena! Our fighters tonight are %s and %s!", f1.GetName(), f2.GetName())
+func arena(f1 Fighter, f2 Fighter) {
+	fmt.Printf("Our fighters tonight are %s and %s!", f1.GetName(), f2.GetName())
+	for {
+		if Rolld(2) == 1 {
+			f1.Act(f2)
+			f2.Act(f1)
+		}
+	}
 }
