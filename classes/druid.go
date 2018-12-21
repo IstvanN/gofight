398035cd4eaff9d9a837d20c634d3df731c24d34
Istvan Nemeth <nemethistvan.ius@gmail.com>
2018-12-21 22:28:12 +0100

    style(main, druid): style imports to match convention

--- classes/druid.go
+++ classes/druid.go
@@ -1,8 +1,6 @@
 package classes
 
-import (
-	"fmt"
-)
+import "fmt"
 
 type druid struct {
 	name                      string
--- main.go
+++ main.go
@@ -4,20 +4,20 @@ import (
 	"fmt"
 	"strings"
 
-	. "github.com/IstvanN/gofight/classes"
+	cl "github.com/IstvanN/gofight/classes"
 )
 
 func main() {
 	fmt.Println("Welcome to GoFight!")
-	var f1 Fighter = CreateRogue("Wolfgraff")
-	var f2 Fighter = CreateDruid("Cenarius")
+	var f1 cl.Fighter = cl.CreateRogue("Wolfgraff")
+	var f2 cl.Fighter = cl.CreateDruid("Cenarius")
 	arena(f1, f2)
 }
 
-func arena(f1 Fighter, f2 Fighter) {
+func arena(f1 cl.Fighter, f2 cl.Fighter) {
 	fmt.Printf("Our fighters tonight are %s and %s!\n", f1.GetName(), f2.GetName())
 	for !eitherOneDead(f1, f2) {
-		if Rolld(2) == 1 {
+		if cl.Rolld(2) == 1 {
 			f1.Act(f2)
 			f2.Act(f1)
 		} else {
@@ -29,11 +29,11 @@ func arena(f1 Fighter, f2 Fighter) {
 	announceWinner(f1, f2)
 }
 
-func eitherOneDead(f1 Fighter, f2 Fighter) bool {
+func eitherOneDead(f1 cl.Fighter, f2 cl.Fighter) bool {
 	return f1.IsDead() || f2.IsDead()
 }
 
-func announceWinner(f1 Fighter, f2 Fighter) {
+func announceWinner(f1 cl.Fighter, f2 cl.Fighter) {
 	if f1.IsDead() {
 		fmt.Printf("Ladies and Gentlemen, the winner is %s!", strings.ToUpper(f2.GetName()))
 	} else {
@@ -41,6 +41,6 @@ func announceWinner(f1 Fighter, f2 Fighter) {
 	}
 }
 
-func status(f1 Fighter, f2 Fighter) {
+func status(f1 cl.Fighter, f2 cl.Fighter) {
 	fmt.Printf("%s has %d hp left.\n%s has %d hp left.\n", f1.GetName(), f1.GetHP(), f2.GetName(), f2.GetHP())
 }
