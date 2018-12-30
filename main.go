d6f2ba7525522bea9b693df05befc49a3069aa3c
Istvan Nemeth <nemethistvan.ius@gmail.com>
2018-12-30 22:23:06 +0100

    fix(unite all the separate .go files in main package)

--- main.go
+++ main.go
@@ -3,21 +3,19 @@ package main
 import (
 	"fmt"
 	"strings"
-
-	cl "github.com/IstvanN/gofight/classes"
 )
 
 func main() {
 	fmt.Println("Welcome to GoFight!")
-	var f1 cl.Fighter = cl.CreateRogue("Wolfgraff")
-	var f2 cl.Fighter = cl.CreateDruid("Cenarius")
+	var f1 Fighter = CreateRogue("Wolfgraff")
+	var f2 Fighter = CreateDruid("Cenarius")
 	arena(f1, f2)
 }
 
-func arena(f1 cl.Fighter, f2 cl.Fighter) {
+func arena(f1 Fighter, f2 Fighter) {
 	fmt.Printf("Our fighters tonight are %s and %s!\n", f1.GetName(), f2.GetName())
 	for !eitherOneDead(f1, f2) {
-		if cl.Rolld(2) == 1 {
+		if Rolld(2) == 1 {
 			f1.Act(f2)
 			f2.Act(f1)
 		} else {
@@ -29,11 +27,11 @@ func arena(f1 cl.Fighter, f2 cl.Fighter) {
 	announceWinner(f1, f2)
 }
 
-func eitherOneDead(f1 cl.Fighter, f2 cl.Fighter) bool {
+func eitherOneDead(f1 Fighter, f2 Fighter) bool {
 	return f1.IsDead() || f2.IsDead()
 }
 
-func announceWinner(f1 cl.Fighter, f2 cl.Fighter) {
+func announceWinner(f1 Fighter, f2 Fighter) {
 	if f1.IsDead() {
 		fmt.Printf("Ladies and Gentlemen, the winner is %s!", strings.ToUpper(f2.GetName()))
 	} else {
@@ -41,6 +39,6 @@ func announceWinner(f1 cl.Fighter, f2 cl.Fighter) {
 	}
 }
 
-func status(f1 cl.Fighter, f2 cl.Fighter) {
+func status(f1 Fighter, f2 Fighter) {
 	fmt.Printf("%s has %d hp left.\n%s has %d hp left.\n", f1.GetName(), f1.GetHP(), f2.GetName(), f2.GetHP())
 }
